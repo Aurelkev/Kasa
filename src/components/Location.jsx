d71491c3c2c8600a951894382151dd01c9fb5e84
Kévin Holleville <archercelest@gmail.com>
2025-03-31 13:23:00 +0200

    Add Cards of location

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,8 +1,10 @@
-import React from "react"; 
+import React from "react";
 import "./Location.css";
 
-function Location (){
-  return <div className="location">Titre de la Location</div>
+function Location() {
+  return <div className="location">
+    <h2 className="location__title">Titre de la location</h2>
+  </div>
 }
 
 export default Location;
--- src/components/LocationGrid.jsx
+++ src/components/LocationGrid.jsx
@@ -8,6 +8,7 @@ function LocationGrid() {
     <Location/>
     <Location/>
     <Location/>
+    
 
   </div>;
 }

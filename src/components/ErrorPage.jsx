52ed887516f87133c210c33bdbdad611acbeee8d
Kévin Holleville <archercelest@gmail.com>
2025-04-01 13:35:41 +0200

    Patch for the errorpage

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -8,7 +8,7 @@ const ErrorPage = () => {
   return (
     <div>
       <Navbar />
-      <div className="about">
+      <div className="error">
         <h2>404</h2>
         <p>Oups! La page que vous demandez n'existe pas.</p>
         <Link to="/" className="home">Retourner sur la page d’accueil</Link>

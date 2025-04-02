cf9be6768d457b0652a9bbed7a3f034dcb137d78
Kévin Holleville <archercelest@gmail.com>
2025-04-02 10:36:36 +0200

    InfoRental Page in Progess

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import Navbar from "./Navbar";
 import "./ErrorPage.scss";
-import Footer from "./Footer";
 import { Link } from "react-router";
 
 const ErrorPage = () => {
--- src/pages/InfoRental.jsx
+++ src/pages/InfoRental.jsx
@@ -1,29 +1,35 @@
 import React from "react";
+import "./InfoRental.scss";
 
 
 function InfoRental() {
   return (
-      <div className="apartment">
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div className="apartment-page">
+    <div className="apartment">
+      <div>
+        <img src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg" alt="" />
+      </div>
+      <div className="apartment__page">
+        <div className="apartment__page__title">
           <h1>Cozy loft on the Canal Saint-Martin</h1>
           <h2>Paris, Île-de-France</h2>
-          <p>Cozy</p>
-          <p>Canal</p>
-          <p>Paris 10</p>
+          <div className="apartment__page__tags">
+            <span>Cozy</span>
+            <span>Canal</span>
+            <span>Paris 10</span>
+          </div>
         </div>
-        <div className="apartment__owner">
+        <div className="apartment__host">
           <h3>Alexandre Dumas</h3>
-        </div>
-        <div className="apartment__description">
-          <p></p>
-        </div>
-        <div className="apartment__description">
-          <p></p>
+          <div className="apartment__host__rate"></div>
         </div>
       </div>
+      <div className="apartment__description">
+        <p></p>
+      </div>
+      <div className="apartment__description">
+        <p></p>
+      </div>
+    </div>
 
   );
 }

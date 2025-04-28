e9ac191b156dea7383a30e7055073f5b81938089
Kévin Holleville <archercelest@gmail.com>
2025-04-28 06:26:19 +0200

    Change Apartment details for collapse

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,25 +1,25 @@
 import React from "react";
-import "./ApartmentDetails.scss";
+import "./Collapse.scss";
 import Chevron from "../FontAwesome/Chevron.jsx";
 import { useState } from "react";
 
-function ApartmentDetails(props) {
+function Collapse(props) {
   const [isVisible, setIsVisible] = useState(false);
   const showContent = () => {
     setIsVisible(!isVisible);
-  }
+  };
 
   return (
-    <div className="apartment__description__details">
-      <p className="apartment__description__title">
+    <div className="collapse__description__details">
+      <p className="collapse__description__title">
         <span>{props.title}</span>
         <Chevron onClick={showContent} className={isVisible ? " " : "up"} />
       </p>
-      {isVisible && <p className="apartment__description__content">
-        {props.content}
-      </p>}
+      {isVisible && (
+        <p className="collapse__description__content">{props.content}</p>
+      )}
     </div>
-  )
+  );
 }
 
-export default ApartmentDetails;
+export default Collapse;
